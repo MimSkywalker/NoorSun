2392c736c4e98ea5868189460e1307adc35a8ad2
Mohammad <mdmdnezhad@gmail.com>
2026-07-18 13:43:46 +0330

    Add Phone validator

--- users/models.py
+++ users/models.py
@@ -1,17 +1,25 @@
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.models import PermissionsMixin
 from django.db import models
+from django.core.validators import RegexValidator
+
+
+phone_validator = RegexValidator(
+    regex=r"^09\d{9}$",
+    message="شماره موبایل باید با 09 شروع شود و 11 رقم باشد."
+)
 
 
 class UserManager(BaseUserManager):
     """
     creat a new user model base on phon number
     """
+
     def create_user(self, phone_number, password=None, **extra_fields):
         if not phone_number:
             raise ValueError('شماره موبایل الزامی است')
         user = self.model(phone_number=phone_number, **extra_fields)
-        user.set_password(password)  
+        user.set_password(password)
         user.save(using=self._db)
         return user
 
@@ -22,7 +30,8 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    phone_number = models.CharField(max_length=11, unique=True)
+    phone_number = models.CharField(
+        max_length=11, unique=True, validators=[phone_validator])
     first_name = models.CharField(max_length=100, blank=True)
     last_name = models.CharField(max_length=100, blank=True)
     is_phone_verified = models.BooleanField(default=False)
@@ -31,8 +40,8 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     objects = UserManager()
 
-    USERNAME_FIELD = 'phone_number' 
-    REQUIRED_FIELDS = [] 
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = []
 
     def __str__(self):
         return self.phone_number
