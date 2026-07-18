154c65610c709739cc1dca332ae3dc0b711a07dc
Mohammad <mdmdnezhad@gmail.com>
2026-07-18 13:02:29 +0330

    Add admin panel for User model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,30 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
+
+
+class UserAdmin(BaseUserAdmin):
+
+    model = User
+    list_display = ('phone_number', 'first_name',
+                    'last_name', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active')
+    search_fields = ('phone_number', 'first_name', 'last_name')
+    ordering = ('phone_number',)
+
+    fieldsets = (
+        (None, {'fields': ('phone_number', 'password')}),
+        ('اطلاعات شخصی', {'fields': ('first_name', 'last_name')}),
+        ('دسترسی‌ها', {'fields': ('is_active', 'is_staff',
+         'is_superuser', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('phone_number', 'password1', 'password2'),
+        }),
+    )
+
+
+admin.site.register(User, UserAdmin)
