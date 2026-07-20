2f140c3debd16e556d561f31d5d090cd7d579af3
Mohammad <mdmdnezhad@gmail.com>
2026-07-20 11:31:59 +0330

    Add admin panel for ProvinceModel, CityModel and CityModel

--- addresses/admin.py
+++ addresses/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Province, City, Address
 
-# Register your models here.
+
+@admin.register(Province)
+class ProvinceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug')
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ('title',)
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('title', 'province', 'slug')
+    list_filter = ('province',)
+    search_fields = ('title', 'province__title')
+    prepopulated_fields = {'slug': ('title',)}
+    autocomplete_fields = ('province',)
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('user', 'title', 'city', 'receiver_name', 'is_default')
+    list_filter = ('is_default', 'city__province')
+    search_fields = ('user__phone_number', 'receiver_name',
+                     'receiver_phone', 'full_address', 'postal_code')
+    autocomplete_fields = ('user', 'city')
