fa0a92dc3d0a4240cae4969ab8c28d1690cd4311
Mohammad <mdmdnezhad@gmail.com>
2026-07-20 11:28:00 +0330

    Add OTP Request Admin

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import User
+from .models import User, OTPRequest
 
 
 class UserAdmin(BaseUserAdmin):
@@ -28,3 +28,12 @@ class UserAdmin(BaseUserAdmin):
 
 
 admin.site.register(User, UserAdmin)
+
+
+@admin.register(OTPRequest)
+class OTPRequestAdmin(admin.ModelAdmin):
+    list_display = ('phone_number', 'purpose', 'is_used', 'attempts', 'created_at', 'expires_at')
+    list_filter = ('purpose', 'is_used')
+    search_fields = ('phone_number',)
+    readonly_fields = ('code', 'created_at', 'expires_at', 'attempts')
+    ordering = ('-created_at',)
