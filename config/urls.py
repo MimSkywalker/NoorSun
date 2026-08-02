e0fd9b4c22e2fe3b121591664d698d515d8d9d4e
Mohammad <mdmdnezhad@gmail.com>
2026-08-02 14:00:45 +0330

    Add home URL

--- config/urls.py
+++ config/urls.py
@@ -19,8 +19,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from products.views import HomeView
+
 
 urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+
     path('admin/', admin.site.urls),
     path('products/', include('products.urls')),
     path('accounts/', include('users.urls')),
@@ -29,4 +33,5 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
